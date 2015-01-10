0166d1b12291821de96e08350b66c582d5ebabe6
mtseman <mtseman@gmail.com>
2015-01-10 12:54:48 -0500

    My Project final ver

---
+++ plot3.R
@@ -0,0 +1,39 @@
+#Project #1 from Coursera class Explortory Data Analysis
+
+
+#set wd and read in the whole file
+setwd("~/R/R_class_John_Hopkins_EDA")
+#designate missing values as "?" by setting na.trings
+allpowerdata<-read.csv('./household_power_consumption.txt',sep=';',header=TRUE,na.strings="?")
+str(allpowerdata)
+head(allpowerdata)
+
+#subset the two days we want to look at and remove incomplete cases
+allpowerdata[,1]<-as.Date(allpowerdata[,1], format="%d/%m/%Y")
+powerdata<-allpowerdata[allpowerdata$Date >= "2007/02/01" & allpowerdata$Date<="2007/02/02",]
+#keep only complete cases, set ?'s -> NA in the read.csv funcion
+good<-complete.cases(powerdata)
+powerdata<-powerdata[good,]
+#change global acive power to numeric class
+powerdata$Global_active_power<-as.numeric(powerdata$Global_active_power)
+
+#need to get a "DateTime" column that is elapsed seconds
+powerdata$DateTime <- paste(powerdata$Date, powerdata$Time) 
+powerdata$DateTime <- as.POSIXct(powerdata$DateTime, format="%Y-%m-%d %H:%M:%S")
+
+#store defaults in old.par
+old.par<-par()
+#run this line to recover defaults
+par(old.par)
+
+
+#construct 3rd plot
+png(file="plot3.png", height=480, width=480)
+par(mar=c(4,4,2,1))
+with(powerdata, plot(DateTime,Sub_metering_1,type="n", 
+                     ylab = "Energy Sub Metering"))  
+with(powerdata, lines(DateTime,Sub_metering_1,col="Black"))
+with(powerdata, lines(DateTime,Sub_metering_2,col="Red"))
+with(powerdata, lines(DateTime,Sub_metering_3,col="Blue"))
+legend("topright", lty=1, col=c("Black","Red","Blue"), cex=0.9,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
